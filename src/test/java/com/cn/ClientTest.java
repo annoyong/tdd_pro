9add3352d1dd43c3bbc5e8c1ddebe7e3ac1676cf
annoyong <yongruli7@icloud.com>
2020-12-31 17:25:05 +0800

    add:新增Cliet找到神秘数字方法和单元测试

---
+++ src/test/java/com/cn/ClientTest.java
@@ -0,0 +1,38 @@
+package com.cn;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+/**
+ * @author yongruli
+ *
+ * CreateTimr 2020-12-31
+ */
+public class ClientTest {
+
+    @Test
+    public void should_returen_secret_num_when_box_is_one() {
+        //given
+        Client client = new Client();
+        int expect = 3229171;
+        //when
+        int actual = client.getSecretNum("1","0");
+
+        //then
+        assertEquals(expect,actual);
+    }
+
+    @Test
+    public void should_returen_secret_num_when_find_all_box() {
+        //given
+        Client client = new Client();
+        int[] expect ={3229171, 1640951, 27941, 2343356, 378555, 2184576, 786903, 35612, 534765, 2033620};
+        //when
+        int[] actual = client.findAllSecretNum();
+
+        //then
+        assertArrayEquals(expect,actual);
+    }
+}
